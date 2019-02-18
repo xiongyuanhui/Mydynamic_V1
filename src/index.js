e17be948e7b310898a8f4431c91bfddea9ea9c30
xiongyuanhui <44856416+xiongyuanhui@users.noreply.github.com>
2019-02-18 16:07:23 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+bui.ready(function() {
+        // 焦点图控件初始化
+    // 焦点图 js 初始化:
+
+    bui.btn({id:".bui-page",handle:".bui-btn,a"}).load();
+     // 列表控件 js 初始化:*/
+
+    //下拉菜单 在顶部右边
+    var barRight = bui.dropdown({
+        id: "#barRight",
+        showArrow: true,
+        width: 200
+    });
+
+})
+
+
+
+
+
+
---
+++ src/js/RequestUserId.js
@@ -0,0 +1,21 @@
+function getUrlVars() {
+    var vars = [],
+        hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var id = getUrlVars()["userid"];
+var ticketid = getUrlVars()["userticket"];
+ function useridreturn() {
+           return(id);
+ }
+
+function userticketreturn() {
+    return(ticketid);
+}
